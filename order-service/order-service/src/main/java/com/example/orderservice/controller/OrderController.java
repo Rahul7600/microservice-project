18afa28ed53966465456ab1afdc5facef2b41617
rahul <rd4989122@gmail.com>
2025-06-15 15:39:47 +0530

    Initial commit of order-user-microservices

---
+++ order-service/order-service/src/main/java/com/example/orderservice/controller/OrderController.java
@@ -0,0 +1,12 @@
+package com.example.orderservice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class OrderController {
+    @GetMapping("/api/orders")
+    public String getOrders() {
+        return "List of Orders from Order Service!";
+    }
+}
---
+++ order-service/order-service/src/test/java/com/example/orderservice/AppTest.java
@@ -0,0 +1,20 @@
+package com.example.orderservice;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    void testApp() {
+        assertTrue(true, "This test should always pass");
+    }
+}
+
